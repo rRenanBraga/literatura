cac703f9fec0314178050a1aca31e2ec90b35328
rRenanBraga <104376037+rRenanBraga@users.noreply.github.com>
2023-11-15 23:20:55 -0300

    COM

--- app/Views/paginas/home.php
+++ app/Views/paginas/home.php
@@ -1,82 +1,100 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seu Título Aqui</title>
+    <link rel="stylesheet" type="text/css" href="<?=URL?>/public/css/pagina.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+    <style>
+        /* Adicione seus estilos personalizados aqui */
+        <style>
+    /* Restante do seu CSS */
+
+    .card {
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+        transition: transform 0.3s ease-in-out;
+        cursor: pointer;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+    }
+
+    .card img {
+        width: 100%; /* Alterado para 100% para garantir que todas as imagens tenham o mesmo tamanho */
+        height: auto;
+        margin-bottom: 10px; /* Espaço entre a imagem e o título */
+    }
+
+    .card-body {
+        padding: 20px;
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .card-title {
+        font-size: 18px;
+        font-weight: bold;
+        margin-bottom: 20px;
+    }
+
+    .card-text {
+        font-size: 14px;
+    }
+</style>
+</head>
+<body>
+
 <div class="container-md">
     <div class="card">
-
-    <link rel="stylesheet" type="text/css" href="<?=URL?>/public/css/pagina.css">
-
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
-
         <div class="card-body">
-          <!--  <h1 class="my-3 text-center"><?= $dados['titulo'] ?></h1> -->
-
-           <!-- <p><b>Um pouco sobre literatura indigena.</b></p> -->
-
-
-           <div vw class="enabled">
-            <div vw-access-button class="active"></div>
-            <div vw-plugin-wrapper>
-            <div class="vw-plugin-top-wrapper"></div>
-        </div>
-    </div>
-        <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
-        <script>
-         new window.VLibras.Widget('https://vlibras.gov.br/app');
-        </script>
-
-           <div class="aviso text-center">
-           <img src="<?=URL?>/public\img\aviso.png" alt="AVISO" style="width: 600px; height: 700px; margin-bottom: 70px;">
-           </div>
-           
-           
-        <div class="destaque-legenda">
-            
-            <span class="linha1"></span>
-            <span class="texto1" style="font-family: fantasy  sans-serif; font-size:20px;">DESTAQUE</span>
-            <span class="linha1"></span>
-           
-        </div>
-        <div class="frase-destaque1">
-             VEJA O QUE PREPARAMOS PARA VOCÊ
-        </div>
-
-           
-           
-           <div class="carousel-container mx-auto mt-4">
-            <!-- IMAGEM DE CIMA DO CARROSEL -->
-           <div class="imagem-lateral">
-           <img src="<?=URL?>/public/img/LATERAL.jpg" alt="Imagem 1" style="width: 600px; height: 100px;">
-           </div>
-               <!-- FIM DA IMAGEM DE CIMA -->
-
-           <!-- INICIO DO CARROSEL -->
-
-           <?= Sessao::mensagem('post') ?>
-<div class="card bg-light">
-    <?php $posts = $dados['posts']; ?>
-    <div class="carousel" id="carousel">
-        <?php foreach ($posts as $post) : ?>   
-            <img class="livro card-img-top" src="<?= URL?>/public/img/<?= $post->obra_imagem?>" >
-        <?php endforeach ?>
-    </div>
-</div>
-            
-          <!-- FIM DO CARROSEL -->
-
-           <!-- IMAGEM DE CIMA DO CARROSEL -->
-            <div class="imagem-lateral">
-            <img src="<?=URL?>/public/img/LATERAL.jpg" alt="Imagem 1" style="width: 400px; height: 100px; margin-bottom: 70px;">
+            <div vw class="enabled">
+                <!-- Seu código para a VLibras aqui -->
             </div>
-</div>
-          <!-- FIM DA IMAGEM DE CIMA -->
 
-    
-        
-              </li>
-                <script src="<?=URL?>/public/js/home.js"></script>
-            </ul>
-     
+            <div class="aviso text-center">
+    <img src="<?=URL?>/public\img\aviso.png" alt="AVISO" class="img-fluid w-50">
+            </div>
+
+            <div class="destaque-legenda">
+                <span class="linha1"></span>
+                <span class="texto1">DESTAQUE</span>
+                <span class="linha1"></span>
+            </div>
+            <div class="frase-destaque1">
+                VEJA O QUE PREPARAMOS PARA VOCÊ
+            </div>
+
+            <div class="row">
+                <?= Sessao::mensagem('post') ?>
+                <?php $posts = $dados['posts']; ?>
+                <?php foreach ($posts as $post) : ?>
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <img class="card-img-top" src="<?= URL?>/public/img/<?= $post->obra_imagem?>" alt="Capa do Livro" style="height: 500px;">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $post->obra_titulo ?></h5>
+                                <p class="card-text"><?= $post->obra_descricao ?></p>
+                                <!-- Adicione mais campos conforme necessário -->
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach ?>
+            </div>
+
+            <!-- Adicione aqui mais conteúdo ou seções conforme necessário -->
+
         </div>
     </div>
 </div>
 
+<!-- Adicione mais scripts ou links necessários aqui -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+<script src="<?=URL?>/public/js/home.js"></script>
+
+</body>
+</html>
